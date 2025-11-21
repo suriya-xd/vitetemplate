473965309f65500288781a282b59f7d77c719732
suriya-xd <suriya.a@cobuildx.ai>
2025-11-21 18:41:30 +0530

    -bare template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import './App.css'
-// src/App.tsx
 function App() {
   return (
-    <div className="app-shell">
-      <div className="app-card">
-        {/* LLM will replace this content for each app */}
-        <h1>App Starter</h1>
-      </div>
+    <div>
     </div>
-  );
+  )
 }
-
 
 export default App
